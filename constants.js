5365d4522450f068d7e589242320ae98c68b94d9
shriom <shriomtripathi33@gmail.com>
2022-11-12 23:33:54 +0530

    update TokenURI

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = '0x5acaFdB98742dce65FD89AddeD22e066C66884F7';
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-cryptodev-web.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
